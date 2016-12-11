b49f18f61dd640945ed6129276baffdf83f731d6
findit2016 <mohamed.amadou159@gmail.com>
2016-12-11 07:36:16 -0500

    Started Navigation Drawer on First Page (Not Done Yet but Base Code to manipulate is done)

--- src/main/java/com/painlessshopping/mohamed/findit/HomeScreen.java
+++ src/main/java/com/painlessshopping/mohamed/findit/HomeScreen.java
@@ -1,21 +1,55 @@
 package com.painlessshopping.mohamed.findit;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private ListView mDrawerList;
+    private String[] mPlanetTitles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        Button mapsBtn = (Button)findViewById(R.id.maps_button);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+        Button mapsBtn = (Button) findViewById(R.id.maps_button);
 
         mapsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,20 +63,87 @@ public class HomeScreen extends AppCompatActivity {
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    startActivity(new Intent(HomeScreen.this, Search.class));
+                startActivity(new Intent(HomeScreen.this, Search.class));
 
             }
         });
-        
+
         Button featuredBtn = (Button) findViewById(R.id.featured_button);
 
         featuredBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    startActivity(new Intent(HomeScreen.this, FeaturedScreen.class));
+                startActivity(new Intent(HomeScreen.this, FeaturedScreen.class));
 
             }
         });
+
+
+    }//End of OnCreate**************************************
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_first_layout) {
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.content_frame
+//                            , new FirstFragment())
+//                    .commit();
+        } else if (id == R.id.nav_second_layout) {
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.content_frame
+//                            , new SecondFragment())
+//                    .commit();
+        } else if (id == R.id.nav_third_layout) {
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.content_frame
+//                            , new ThirdFragment())
+//                    .commit();
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}//End of Class
