13633d4658eaa64ed864af47a106b06af9def93d
findit2016 <mohamed.amadou159@gmail.com>
2017-01-01 20:15:03 -0500

    Navigation Drawer Added to All Search Activities

--- src/main/java/com/painlessshopping/mohamed/findit/ClothingSearch.java
+++ src/main/java/com/painlessshopping/mohamed/findit/ClothingSearch.java
@@ -10,7 +10,11 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -58,7 +62,7 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ClothingSearch extends AppCompatActivity {
+public class ClothingSearch extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     private static Context context;
     ProgressDialog dialog;
@@ -93,6 +97,20 @@ public class ClothingSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_search);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         context = this;
 
         ListView listView=(ListView)findViewById(R.id.listView);
@@ -277,7 +295,51 @@ public class ClothingSearch extends AppCompatActivity {
         adapter.notifyDataSetChanged();
 
     }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-}
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_search) {
+            startActivity(new Intent(ClothingSearch.this, HomeScreen.class));
+
+        }  else if (id == R.id.nav_my_cart) {
+            startActivity(new Intent(ClothingSearch.this, MyCart.class));
+
+        } else if (id == R.id.nav_featured_stores) {
+            startActivity(new Intent(ClothingSearch.this, FeaturedScreen.class));
+
+        } else if (id == R.id.nav_language_settings) {
+
+
+        } else if (id == R.id.nav_location_settings) {
+            startActivity(new Intent(ClothingSearch.this, MapsActivity.class));
+
+        } else if (id == R.id.nav_display) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}//End of Class
 
 
