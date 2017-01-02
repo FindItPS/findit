388cf92812726be40a56c9fc4d52a2bc383673c7
findit2016 <mohamed.amadou159@gmail.com>
2017-01-01 19:52:57 -0500

    Navigation Drawer Added to Tech Search

--- src/main/java/com/painlessshopping/mohamed/findit/TechSearch.java
+++ src/main/java/com/painlessshopping/mohamed/findit/TechSearch.java
@@ -9,8 +9,13 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -58,7 +63,7 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class TechSearch extends AppCompatActivity {
+public class TechSearch extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static Context context;
     ProgressDialog dialog;
@@ -93,6 +98,20 @@ public class TechSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_search);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         context = this;
 
         ListView listView=(ListView)findViewById(R.id.listView);
@@ -162,6 +181,7 @@ public class TechSearch extends AppCompatActivity {
 
         }
     }
+
 
     /**
      * A placeholder fragment containing a simple view.
@@ -278,6 +298,53 @@ public class TechSearch extends AppCompatActivity {
 
     }
 
-}
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_search) {
+            startActivity(new Intent(TechSearch.this, HomeScreen.class));
+
+        }  else if (id == R.id.nav_my_cart) {
+            startActivity(new Intent(TechSearch.this, MyCart.class));
+
+        } else if (id == R.id.nav_featured_stores) {
+            startActivity(new Intent(TechSearch.this, FeaturedScreen.class));
+
+        } else if (id == R.id.nav_language_settings) {
+
+
+        } else if (id == R.id.nav_location_settings) {
+            startActivity(new Intent(TechSearch.this, MapsActivity.class));
+
+        } else if (id == R.id.nav_display) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}//End of Class
+
+
 
 
