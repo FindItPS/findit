1388f1220aaa718c425eed6380717e6f669857dc
findit2016 <mohamed.amadou159@gmail.com>
2017-01-01 20:28:10 -0500

    Navigation Drawer Added to Featured Stores Screen

--- src/main/java/com/painlessshopping/mohamed/findit/FeaturedScreen.java
+++ src/main/java/com/painlessshopping/mohamed/findit/FeaturedScreen.java
@@ -6,9 +6,15 @@ package com.painlessshopping.mohamed.findit;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -22,12 +28,26 @@ import android.widget.Toast;
 
 import com.google.android.gms.appdatasearch.Feature;
 
-public class FeaturedScreen extends AppCompatActivity {
+public class FeaturedScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener   {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_featured_screen);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
     GridView gridview = (GridView) findViewById(R.id.gridview);
     gridview.setAdapter(new FeaturedStoresAdapter(this));
@@ -41,5 +61,49 @@ public class FeaturedScreen extends AppCompatActivity {
     });
 }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-}
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_search) {
+            startActivity(new Intent(FeaturedScreen.this, HomeScreen.class));
+
+        }  else if (id == R.id.nav_my_cart) {
+            startActivity(new Intent(FeaturedScreen.this, MyCart.class));
+
+        } else if (id == R.id.nav_featured_stores) {
+            startActivity(new Intent(FeaturedScreen.this, FeaturedScreen.class));
+
+        } else if (id == R.id.nav_language_settings) {
+
+
+        } else if (id == R.id.nav_location_settings) {
+            startActivity(new Intent(FeaturedScreen.this, MapsActivity.class));
+
+        } else if (id == R.id.nav_display) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}//End of Class
