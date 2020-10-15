9bd19b5ada490b105c6eabbedbc647d1bc921ae2
GitHub <mohamed.amadou159@gmail.com>
2020-10-15 18:43:12 -0400

    Refactor clothing search imports

--- src/main/java/com/painlessshopping/mohamed/findit/CartAdapter.java
+++ src/main/java/com/painlessshopping/mohamed/findit/CartAdapter.java
@@ -3,7 +3,7 @@ package com.painlessshopping.mohamed.findit;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.design.widget.Snackbar;
+import com.google.android.material.snackbar.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
