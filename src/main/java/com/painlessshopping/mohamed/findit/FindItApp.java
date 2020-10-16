ba20a1fbfc533e080a917dc6439286de90846ae2
GitHub <mohamed.amadou159@gmail.com>
2020-10-15 20:00:50 -0400

    Override multidex application

---
+++ src/main/java/com/painlessshopping/mohamed/findit/FindItApp.java
@@ -0,0 +1,10 @@
+package com.painlessshopping.mohamed.findit;
+
+import android.app.Application;
+
+public class FindItApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
